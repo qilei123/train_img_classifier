0866b67c6f0e9fe79c0a9fa96bad60af6a100a6d
qilei123 <qileimail123@gmail.com>
2020-12-08 02:26:23 -0500

    set epoch

--- transfer_learning.py
+++ transfer_learning.py
@@ -356,7 +356,7 @@ data_transforms = {
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(input_size),
+        transforms.Resize(256),
         transforms.CenterCrop(input_size),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
