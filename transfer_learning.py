40de1611ebfd8a9801de382c355ea98831f19fe2
qilei123 <qileimail123@gmail.com>
2020-12-08 03:32:15 -0500

    set opt

--- transfer_learning.py
+++ transfer_learning.py
@@ -354,16 +354,16 @@ if os.path.exists(outputdir+'/latest.model'):
 data_transforms = {
     'train': transforms.Compose([
         #transforms.RandomResizedCrop(input_size),
-        transforms.Resize(input_size),
+        transforms.Resize(256),
+        transforms.CenterCrop(input_size),
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
-        #transforms.Resize(256),
-        #transforms.CenterCrop(input_size),
-        transforms.Resize(input_size),
+        transforms.Resize(256),
+        transforms.CenterCrop(input_size),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
