caf7c95e8f89267a961b90137bc79f19173c1321
qilei123 <qileimail123@gmail.com>
2020-12-09 03:11:36 -0500

    test

--- inference_api.py
+++ inference_api.py
@@ -650,11 +650,11 @@ if __name__ == "__main__":
     videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed2/"
     videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
     process_4_situation_videos_gray(videos_folder_dir,model_dir,model_name,videos_result_folder)
-    '''
+    
     videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed_changjing20/"
     videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
     process_4_situation_videos_gray(videos_folder_dir,model_dir,model_name,videos_result_folder)
-    
+    '''
     '''
     try:
         videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed/"
@@ -674,11 +674,11 @@ if __name__ == "__main__":
 
     model_name="mobilenetv2"
     model_dir = "/data2/qilei_chen/DATA/GI_4_NEW/grayscale/"+model_name+"/best.model"
-    '''
+    
     videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed/"
     videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
     process_4_situation_videos_gray(videos_folder_dir,model_dir,model_name,videos_result_folder)
-
+    '''
     videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed2/"
     videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
     process_4_situation_videos_gray(videos_folder_dir,model_dir,model_name,videos_result_folder)
