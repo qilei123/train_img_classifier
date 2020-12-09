be2807ee6e148e25880de281b29ae7b48fd27a11
qilei123 <qileimail123@gmail.com>
2020-12-09 03:05:43 -0500

    video

--- inference_api.py
+++ inference_api.py
@@ -642,11 +642,11 @@ import threading
 if __name__ == "__main__":
     model_name="densenet161"
     model_dir = "/data2/qilei_chen/DATA/GI_4_NEW/grayscale/"+model_name+"/best.model"
-    '''
+    
     videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed/"
     videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
     process_4_situation_videos_gray(videos_folder_dir,model_dir,model_name,videos_result_folder)
-
+    '''
     videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed2/"
     videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
     process_4_situation_videos_gray(videos_folder_dir,model_dir,model_name,videos_result_folder)
@@ -654,6 +654,7 @@ if __name__ == "__main__":
     videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed_changjing20/"
     videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
     process_4_situation_videos_gray(videos_folder_dir,model_dir,model_name,videos_result_folder)
+    '''
     '''
     try:
         videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed/"
@@ -668,7 +669,8 @@ if __name__ == "__main__":
     except:
         print("Error: unable to start thread")
 
-    pass
+    
+    '''
 
     model_name="mobilenetv2"
     model_dir = "/data2/qilei_chen/DATA/GI_4_NEW/grayscale/"+model_name+"/best.model"
@@ -685,6 +687,7 @@ if __name__ == "__main__":
     videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
     process_4_situation_videos_gray(videos_folder_dir,model_dir,model_name,videos_result_folder)
     '''
+    '''
     try:
         videos_folder_dir = "/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed/"
         videos_result_folder = os.path.join(videos_folder_dir,"grayscale_"+model_name)
@@ -697,6 +700,6 @@ if __name__ == "__main__":
         threading.Thread( target=process_4_situation_videos_gray, args=(videos_folder_dir,model_dir,model_name,videos_result_folder) ).start()
     except:
         print("Error: unable to start thread")
-
+    '''
     pass
 
