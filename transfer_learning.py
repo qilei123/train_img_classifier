bf4ba0b893af01399455a270dd4e932b845d9166
qilei123 <qileimail123@gmail.com>
2020-12-11 12:15:40 -0500

    test

--- transfer_learning.py
+++ transfer_learning.py
@@ -71,7 +71,7 @@ parser.add_argument('--datadir', '-d', help='set the training dataset', default=
 parser.add_argument('--outputdir', '-o', help='set the model output dir', default="/data1/qilei_chen/DATA/gastro/binary/test1")
 parser.add_argument('--grayscale', '-gr', help='transformer with grayscale', default=False)
 parser.add_argument('--focalloss', '-fl', help='loss function is focaloss', default=False)
-
+parser.add_argument('--withoutflip', '-wf', help='without flip', default=False)
 args = parser.parse_args()
 
 
@@ -366,7 +366,21 @@ if with_grayscale:
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
-    } 
+    }
+elif without_flip:
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.Resize((input_size,input_size)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize((input_size,input_size)),
+            #transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }     
 else:   
     data_transforms = {
         'train': transforms.Compose([
