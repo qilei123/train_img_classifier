885ea87a0ab13766482ce2403af53416d62bd6d7
qilei123 <qileimail123@gmail.com>
2020-12-22 21:42:33 -0500

    test

--- show_confusionmatrix.py
+++ show_confusionmatrix.py
@@ -23,16 +23,18 @@ def show_records(records_dir):
     print(count)
     print(counts)
 
-projects = {"binary/test1":[0,1],"multilabel5":[0,1,2,3,4]}
-projects = {"binary/test1":[0,1]}
-projects = {"multilabel5/best_test":[0,1,2,3,4]}
-model_names = ["vgg11","densenet121","densenet161","inception3","mobilenetv2"]
+projects = {"binary/test":['disease_free', 'diseased'],
+            "3categories/test":['disease_free', 'diseased_mild', 'diseased_severe'],
+            "5categories/test":['cancer', 'disease_free', 'early_cancer', 'erosive', 'ulcer'][]}
+#projects = {"binary/test1":[0,1]}
+#projects = {"multilabel5/best_test":[0,1,2,3,4]}
+#model_names = ["vgg11","densenet121","densenet161","inception3","mobilenetv2"]
 model_names = ["vgg11_bn","densenet121","densenet161","mobilenetv2"]
 for key in projects:
     for model_name in model_names:
         print(model_name)
         for label in projects[key]:
-            records_dir = os.path.join("/data1/qilei_chen/DATA/gastro",key,model_name,"best.model_"+str(label)+".txt")
+            records_dir = os.path.join("/data1/qilei_chen/DATA/gastro_v2",key,model_name,"best.model_"+str(label)+".txt")
             show_records(records_dir)
 #show_records("/data1/qilei_chen/DATA/gastro/binary/vgg11/best.model_0.txt")
 #show_records("/data1/qilei_chen/DATA/gastro/binary/vgg11/best.model_1.txt")
