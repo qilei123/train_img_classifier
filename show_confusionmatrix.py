38eac3d192b6b54e89205941e0876cb6e2d3aa4e
qilei123 <qileimail123@gmail.com>
2021-01-05 22:57:08 -0500

    test

--- show_confusionmatrix.py
+++ show_confusionmatrix.py
@@ -23,13 +23,13 @@ def show_records(records_dir):
     print(count)
     print(counts)
 
-projects = {"binary/test1":['disease_free', 'diseased'],
-            "3categories/test1":['disease_free', 'diseased_mild', 'diseased_severe'],
-            "5categories/test1":['cancer', 'disease_free', 'early_cancer', 'erosive', 'ulcer']}
+projects = {"binary/test2":['disease_free', 'diseased'],
+            "3categories/test2":['disease_free', 'diseased_mild', 'diseased_severe'],
+            "5categories/test2":['cancer', 'disease_free', 'early_cancer', 'erosive', 'ulcer']}
 #projects = {"binary/test1":[0,1]}
 #projects = {"multilabel5/best_test":[0,1,2,3,4]}
 #model_names = ["vgg11","densenet121","densenet161","inception3","mobilenetv2"]
-model_names = ["vgg11","densenet121","densenet161","mobilenetv2"]
+model_names = ["vgg11_500","densenet121_500","densenet161_500","mobilenetv2_500","resnet50_500"]
 for key in projects:
     for model_name in model_names:
         print(model_name)
