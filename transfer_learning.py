917558b05dd57fdce5ebf2652c4bb4da8dc9ee33
qilei123 <qileimail123@gmail.com>
2021-02-17 15:19:39 -0500

    test

--- transfer_learning.py
+++ transfer_learning.py
@@ -163,6 +163,7 @@ def initialize_model(model_name, num_classes, use_pretrained=True):
         in_num_ftrs = model_ft.classifier[0].in_features
         out_num_ftrs = model_ft.classifier[0].out_features
         print(in_num_ftrs)
+        print(out_num_ftrs)
         model_ft.classifier[0] = nn.Linear(in_num_ftrs,out_num_ftrs/supress_num)
         
         in_num_ftrs = model_ft.classifier[3].in_features
