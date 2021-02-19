990ee05984deec184b10b8131a6fcbaae46fe556
qilei123 <qileimail123@gmail.com>
2021-02-19 00:06:37 -0500

    test

--- testCLAHE.py
+++ testCLAHE.py
@@ -23,7 +23,7 @@ lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
 
 lab_planes = cv2.split(lab)
 
-clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
+clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(16,16))
 
 lab_planes[0] = clahe.apply(lab_planes[0])
 
