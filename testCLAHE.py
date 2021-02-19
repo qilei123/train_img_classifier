2c7b0d50b41643bc3d03927b2c41aa615404e42e
qilei123 <qileimail123@gmail.com>
2021-02-19 00:01:10 -0500

    test

--- testCLAHE.py
+++ testCLAHE.py
@@ -1,15 +1,15 @@
 import numpy as np
 import cv2 as cv
 
-test_img_dir = ""
+test_img_dir = "/data1/qilei_chen/DATA/ROP_DATASET/images/00c9da2807dfa3115e7fb96c57da460b.png"
 
-img = cv.imread('wiki.jpg',0)
+img = cv.imread(test_img_dir)
 equ = cv.equalizeHist(img)
 res = np.hstack((img,equ)) #stacking images side-by-side
-cv.imwrite('test.jpg',res)
+cv.imwrite('equalhist.jpg',res)
 
 
-img = cv.imread('tsukuba_l.png',0)
+img = cv.imread(test_img_dir)
 # create a CLAHE object (Arguments are optional).
 clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 cl1 = clahe.apply(img)
