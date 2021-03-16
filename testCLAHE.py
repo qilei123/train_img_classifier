3dbbfda1413cf6088ce426ca5d10fce41bfa0b08
qilei123 <qileimail123@gmail.com>
2021-03-16 00:20:28 -0400

    test

--- testCLAHE.py
+++ testCLAHE.py
@@ -7,14 +7,14 @@ test_img_dir = "/data1/qilei_chen/DATA/erosive/images/0d8b52cc-2fbb-413e-b513-70
 img = cv.imread(test_img_dir,0)
 equ = cv.equalizeHist(img)
 res = np.hstack((img,equ)) #stacking images side-by-side
-cv.imwrite('equalhist.jpg',res)
+cv.imwrite('/data1/qilei_chen/DATA/erosive/temp/equalhist.jpg',res)
 
 
 img = cv.imread(test_img_dir,0)
 # create a CLAHE object (Arguments are optional).
 clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 cl1 = clahe.apply(img)
-cv.imwrite('clahe_2.jpg',cl1)
+cv.imwrite('/data1/qilei_chen/DATA/erosive/temp/clahe_2.jpg',cl1)
 
 
 img = cv2.imread(test_img_dir)
@@ -26,7 +26,7 @@ img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
 
 # convert the YUV image back to RGB format
 img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-cv.imwrite('equalhist_bgr.jpg',img_output)
+cv.imwrite('/data1/qilei_chen/DATA/erosive/temp/equalhist_bgr.jpg',img_output)
 
 bgr = cv2.imread(test_img_dir)
 
@@ -42,4 +42,4 @@ lab = cv2.merge(lab_planes)
 
 bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
 
-cv.imwrite('clahe_bgr.jpg',bgr)
+cv.imwrite('/data1/qilei_chen/DATA/erosive/temp/clahe_bgr.jpg',bgr)
