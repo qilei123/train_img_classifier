ee2b5443acc2a4b12810f7d31df28c5188ac49d1
qilei123 <qileimail123@gmail.com>
2021-05-28 13:40:12 -0400

    test

--- inference_api.py
+++ inference_api.py
@@ -707,7 +707,7 @@ def create_confusion_matrix():
 
 def test_videos():
     model_name="mobilenetv2"
-    dataset_name = "5class_alex_manual"
+    dataset_name = "5class_scene_alex_manual"
     #dataset_name = "5class_scene_combine_2_fine_2_3"
     model_dir = "/data2/qilei_chen/DATA/"+dataset_name+"/work_dir/mobilenetv2_2/best.model"
     
