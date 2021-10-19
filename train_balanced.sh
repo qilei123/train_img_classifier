3d6c0cb8285901b41ff2919124f1937315773ce3
qilei123 <qileimail123@gmail.com>
2021-10-19 13:45:47 -0400

    test

--- train_balanced.sh
+++ train_balanced.sh
@@ -1,6 +1,8 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=2
 DATA_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/
 OUTPUT_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/work_dir_balanced
-python transfer_learning.py -m ViT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 32 -is True
-python transfer_learning.py -m Nest -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 4 -is True
-python transfer_learning.py -m TwinsSVT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 4 -is True
+
+python transfer_learning.py -m resnet50 -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 32 -is True
+#python transfer_learning.py -m ViT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 32 -is True
+#python transfer_learning.py -m Nest -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 4 -is True
+#python transfer_learning.py -m TwinsSVT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 4 -is True
