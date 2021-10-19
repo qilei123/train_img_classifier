c0e14dcb1ca9e4afc58a252b4ff324c975a41385
qilei123 <qileimail123@gmail.com>
2021-10-19 14:05:26 -0400

    test

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 DATA_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/
 OUTPUT_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/work_dir
 #python transfer_learning.py -m resnet50 -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 32
