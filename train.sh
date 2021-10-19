8e1275742300683d37f312c85b37ed221407ecf1
qilei123 <qileimail123@gmail.com>
2021-10-19 00:52:32 -0400

    test

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ DATA_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/
 OUTPUT_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/work_dir
 #python transfer_learning.py -m ViT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 32
 #python transfer_learning.py -m ViT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 32 -is True
-python transfer_learning.py -m Nest -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 8
-python transfer_learning.py -m Nest -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 8 -is True
-python transfer_learning.py -m TwinsSVT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 8
-python transfer_learning.py -m TwinsSVT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 8 -is True
+python transfer_learning.py -m Nest -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 2
+python transfer_learning.py -m Nest -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 2 -is True
+python transfer_learning.py -m TwinsSVT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 2
+python transfer_learning.py -m TwinsSVT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 2 -is True
