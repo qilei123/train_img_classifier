f6acc256601113d32c0056f42d5893948884eddf
qilei123 <qileimail123@gmail.com>
2021-10-26 19:26:09 -0400

    test

--- train_endoscope3.sh
+++ train_endoscope3.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=2
 export DATA_DIR=/data3/qilei_chen/DATA/endoscope3/
 export OUTPUT_DIR=/data3/qilei_chen/DATA/endoscope3/work_dir_balanced
-python transfer_learning.py -m resnet50 -d $DATA_DIR -o $OUTPUT_DIR -c 3 -b 128 # -is True
+python transfer_learning.py -m mobilenetv2 -d $DATA_DIR -o $OUTPUT_DIR -c 3 -b 64 # -is True
