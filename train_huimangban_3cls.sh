30c7614764eb04e7c171b53643474ab922ea93fd
qilei123 <qileimail123@gmail.com>
2021-11-18 14:33:31 -0500

    test

--- train_huimangban_3cls.sh
+++ train_huimangban_3cls.sh
@@ -8,3 +8,13 @@ export DATA_DIR=/data3/qilei_chen/DATA/huimangban_3cls_cropped/
 export OUTPUT_DIR=/data3/qilei_chen/DATA/huimangban_3cls_cropped/work_dir #_balanced
 python transfer_learning.py -m mobilenetv2 -d $DATA_DIR -o $OUTPUT_DIR -c 3 -b 64 #-is True
 python transfer_learning.py -m resnet50 -d $DATA_DIR -o $OUTPUT_DIR -c 3 -b 64 #-is True
+
+export DATA_DIR=/data3/qilei_chen/DATA/huimangban_3cls/
+export OUTPUT_DIR=/data3/qilei_chen/DATA/huimangban_3cls/work_dir_balanced
+python transfer_learning.py -m mobilenetv2 -d $DATA_DIR -o $OUTPUT_DIR -c 3 -b 64 -is True
+python transfer_learning.py -m resnet50 -d $DATA_DIR -o $OUTPUT_DIR -c 3 -b 64 -is True
+
+export DATA_DIR=/data3/qilei_chen/DATA/huimangban_3cls_cropped/
+export OUTPUT_DIR=/data3/qilei_chen/DATA/huimangban_3cls_cropped/work_dir_balanced
+python transfer_learning.py -m mobilenetv2 -d $DATA_DIR -o $OUTPUT_DIR -c 3 -b 64 -is True
+python transfer_learning.py -m resnet50 -d $DATA_DIR -o $OUTPUT_DIR -c 3 -b 64 -is True
