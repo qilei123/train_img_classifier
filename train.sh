be53536cbbd3340500a626320ec5ca47800339ff
qilei123 <qileimail123@gmail.com>
2022-03-29 16:09:07 -0400

    test

--- train.sh
+++ train.sh
@@ -1,10 +1,16 @@
 export CUDA_VISIBLE_DEVICES=0
-export DATA_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/
-export OUTPUT_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/work_dir
+#export DATA_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/
+#export OUTPUT_DIR=/data2/qilei_chen/DATA/gastro_position_clasification/work_dir
 #python transfer_learning.py -m resnet50 -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 32
-python transfer_learning.py -m ViT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 128 -l 0.1 -p True
+#python transfer_learning.py -m ViT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 128 -l 0.1 -p True
 #python transfer_learning.py -m ViT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 32 -is True
 #python transfer_learning.py -m Nest -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 4
 #python transfer_learning.py -m Nest -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 4 -is True
 #python transfer_learning.py -m TwinsSVT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 4
 #python transfer_learning.py -m TwinsSVT -d $DATA_DIR -o $OUTPUT_DIR -c 12 -b 4 -is True
+
+#new innner and outside the body
+export DATA_DIR=/data/qilei/.DATASETS/Endoskop4INOUT/
+export OUTPUT_DIR=${DATA_DIR}work_dir
+
+python transfer_learning.py -m ViT -d $DATA_DIR -o $OUTPUT_DIR -c 2 -b 128 -l 0.1 -p True
